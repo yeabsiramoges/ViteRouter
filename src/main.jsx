ef73c2fe38c951441f400edb35a1cd5f67e6ce95
yeabsiramoges <yeab@mit.edu>
2023-08-27 14:28:19 -0700

    created creator component

--- src/main.jsx
+++ src/main.jsx
@@ -8,12 +8,17 @@ import {
 import './index.css'
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
+import Creator from "./routes/creator";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: "creators/:creatorId",
+    element: <Creator />,
   },
 ]);
 
